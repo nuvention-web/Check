d3af1ad9fce5831cc79026a2d817ada568b76ead
Andy Bayer <andrewbayer2016@u.northwestern.edu>
2015-01-16 10:03:02 -0600

    initial commit

---
+++ app_landing_page/pages/static/js/main.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+    $('#email-form').on('submit', function(event){
+        event.preventDefault();
+        create_post();
+    });
+});
+
+// AJAX for posting
+function create_post() {
+    console.log("create post is working!") // sanity check
+    $.ajax({
+        url : "submit_email/", // the endpoint
+        type : "POST", // http method
+        data : { 
+            email : $('#email-text').val(),
+            csrfmiddlewaretoken: csrftoken,
+        }, // data sent with the post request
+
+        // handle a successful response
+        success : function(json) {
+            $('#email-text').val(''); // remove the value from the input
+            console.log(json); // log the returned json to the console
+            thanks_fade_in()
+        },
+
+        // handle a non-successful response
+        error : function(xhr,errmsg,err) {
+            $('#results').html("<div class='alert-box alert radius' data-alert>Oops! We have encountered an error: "+errmsg+
+                " <a href='#' class='close'>&times;</a></div>"); // add the error to the dom
+            console.log(xhr.status + ": " + xhr.responseText); // provide a bit more info about the error to the console
+        }
+    });
+};
+
+function thanks_fade_in() {
+    $('#disclaimer').fadeOut(500, function callback() {
+        $("#thank-you").fadeIn(500);
+    });
+
+}
---
+++ app_landing_page/pages/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# from pages.forms import EmailForm
+from pages.models import Email
+
+import json
+
+def submit_email(request):
+    response_data = {}
+    if request.method == 'POST':
+        # form = EmailForm(request.POST)
+        form_email = request.POST.get('email')
+        email = Email(address=form_email)
+        # print email.address # sanity check
+        email.save()
+
+        response_data['result'] = 'Submit email successful!'
+
+        return HttpResponse(          
+            json.dumps(response_data),
+            content_type="application/json"
+        )
+
+    else:
+        return HttpResponse(
+            json.dumps({"nothing to see": "this isn't happening"}),
+            content_type="application/json"
+        )
+
+
+
+
+
