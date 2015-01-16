165fe5a247c7395ade2cdbb43afa9836272a4289
Andy Bayer <andrewbayer2016@u.northwestern.edu>
2015-01-16 10:29:48 -0600

    procfile, requirements change

--- app_landing_page/landingpage/settings.py
+++ app_landing_page/landingpage/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	'pages',
+    'pages',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -77,6 +77,9 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
