02814a87d8ca04f5a86529eae1c1c557cb6eb1cf
Andy Bayer <andrewbayer2016@u.northwestern.edu>
2015-01-16 10:50:02 -0600

    adjust requirements for updated db

--- app_landing_page/landingpage/settings.py
+++ app_landing_page/landingpage/settings.py
@@ -80,7 +80,8 @@ USE_TZ = True
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
-DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
+DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
+DATABASES['default']['NAME'] = os.path.join(BASE_DIR, 'db.sqlite3')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
